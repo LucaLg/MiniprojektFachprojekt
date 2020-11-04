1a807bca36963ed8eb5d8aa98aef2c3cad10d431
luca <lucal.gno@gmail.com>
2020-11-04 16:40:54 +0100

    SchiessenBewegenWandTileMap

---
+++ Miniprojekt/Assets/Scripts/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Bullet : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       Collider2D other = collision.collider;
+        switch (other.gameObject.tag)
+        {
+
+            case "Wand":
+
+                Destroy(gameObject);
+                break;
+            case "Gegner":
+                //Enemys take Damage other.GameObject.GetComponent<MyEnemyScript>().TakeDamage();
+                
+                Destroy(gameObject);
+                break;
+        }
+
+    }
+   /* void OnTriggerEnter2D(Collider2D other)
+    {
+
+        switch (other.gameObject.tag)
+        {
+
+            case "Wand":
+                
+                Destroy(gameObject);
+                break;
+            case "Gegner":
+                //Enemys take Damage other.GameObject.GetComponent<MyEnemyScript>().TakeDamage();
+                GameObject enemy = other.gameObject;
+                Destroy(enemy, 0.2f);
+                Destroy(gameObject);
+                break;
+        }
+
+    }*/
+}
---
+++ Miniprojekt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public Camera cam;
+    public Transform firePoint;
+    
+    public GameObject bulletPrefab;
+    public float bulletForce;
+    Vector2 movement;
+    Vector2 mousePos;
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement Vector zwischen speichern
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        // Mouse Position zum Zielen zwischenspeichern
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+    private void FixedUpdate()
+    {
+        //Bewegung aktuelle Position + Vector wo wir hin wollen * Geschwindigkeit des Characters
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+       
+        /*
+         * Maus Position - aktuelle Position = Vector von Spieler zur Maus Position
+         * Z Roatation Winkel um Character zu drehen Mathf Rad2Deg um Grad der Drehung zu bekommen
+         * */
+        Vector2 lookDir = mousePos - rb.position;
+        float zRotation = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = zRotation;   
+    }
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position , firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
