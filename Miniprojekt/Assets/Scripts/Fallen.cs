46a2e08ab4dcbb864bed39d79dae2ae3f27a6bde
luca <lucal.gno@gmail.com>
2020-11-16 20:05:16 +0100

    Gegnerspawn angepasst
    Es spawnen 2 GegnerTypen und spawnen in Umgebung sollte nicht mehr moglich sein

---
+++ Miniprojekt/Assets/Scripts/Fallen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Fallen : MonoBehaviour
+{
+    public bool isActive;
+    private int fallenStatus;
+    public Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fallenStatus = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Color c=Color.white;
+        if (fallenStatus == 1)
+        {
+            c = Color.yellow;
+        }else if(fallenStatus == 2)
+        {
+            c = Color.red;
+        }
+
+
+        tilemap.SetTileFlags(new Vector3Int(-15, -6, 0), TileFlags.None);
+        tilemap.SetColor(new Vector3Int(-15, -6, 0), Color.red);
+        tilemap.RefreshAllTiles();
+
+        //StartCoroutine(FallenStatus());
+
+    }
+    IEnumerator FallenStatus()
+    {
+        
+        yield return new WaitForSeconds(3);
+        if (fallenStatus <2)
+        {
+            fallenStatus++;
+        }
+        if(fallenStatus == 2)
+        {
+            fallenStatus = 0;
+        }
+    }
+}
--- Miniprojekt/Assets/Scripts/GegnerSpawner.cs
+++ Miniprojekt/Assets/Scripts/GegnerSpawner.cs
@@ -4,28 +4,47 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class GegnerSpawner : MonoBehaviour
 {
-    public GameObject enemy;
-    public float xPos, yPos;
+    public GameObject enemySchnell;
+    public GameObject enemyStandard;
+    public int xPos, yPos;
     public float spawnRate = 2f;
     float nextSpawn;
-    public int normalGegnerAnzahl;
+    public int GegnerAnzahl;
     public Tilemap wand;
     public Tile tile;
     public GameObject door;
     public Tilemap umgebung;
+    private bool standard=true;
     void Update()
     {
-
-        if (normalGegnerAnzahl > 0 && Time.time > nextSpawn)
+        if(GegnerAnzahl <= 10)
+        {
+            spawnRate = 0.5f;
+        }
+        if (GegnerAnzahl > 0 && Time.time > nextSpawn)
         {
             nextSpawn = Time.time + spawnRate;
-            xPos = Random.Range(-20f, 4f);
-            yPos = Random.Range(-8f, 10f);
-            TileBase spawnTile = umgebung.GetTile(new Vector3Int(System.Convert.ToInt32(xPos), System.Convert.ToInt32(yPos), 0));
-            Instantiate(enemy, new Vector2(xPos, yPos), Quaternion.identity);
-            normalGegnerAnzahl = normalGegnerAnzahl-1;
+            xPos = System.Convert.ToInt32(Random.Range(-20f, 4f));
+            yPos = System.Convert.ToInt32(Random.Range(-8f, 10f));
+            TileBase spawnTile = umgebung.GetTile(new Vector3Int(xPos, yPos, 0));
+             while(spawnTile != null) {
+                xPos = rerollX();
+                yPos = rerollY();
+                spawnTile = umgebung.GetTile(new Vector3Int(xPos, yPos, 0));
+            }
+            if (standard)
+            {
+                Instantiate(enemyStandard, new Vector2(xPos, yPos), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(enemySchnell, new Vector2(xPos, yPos), Quaternion.identity);
+            }
+            standard = !standard;
+            GegnerAnzahl = GegnerAnzahl - 1;
+
         }
-        else if (normalGegnerAnzahl==0)
+        else if (GegnerAnzahl==0)
         {
             //Spawn Door
             Vector3Int vec1 = new Vector3Int(-8, 12, 0);
@@ -38,5 +57,13 @@ public class GegnerSpawner : MonoBehaviour
             wand.SetTile(vec4, tile);
             door.SetActive(true);
         }
+    }
+    int rerollX()
+    {
+        return System.Convert.ToInt32(Random.Range(-20f, 4f)); 
+    }
+    int rerollY()
+    {
+        return System.Convert.ToInt32(Random.Range(-8f, 10f));
     }
 }
