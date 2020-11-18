e264416ba613bf233a480169b7f1dd7b012f9a14
luca <lucal.gno@gmail.com>
2020-11-18 13:23:09 +0100

    DontDestroyOnLoadBossRaum
    Der Bossraum Funktioniert mit Dont Destroy On load . Man koennt eine neue Szene fuer den Zweiten Raum erstellen mit A* Star objekt und neuem SpielController

--- Miniprojekt/Assets/Scripts/DoorScript.cs
+++ Miniprojekt/Assets/Scripts/DoorScript.cs
@@ -7,15 +7,28 @@ public class DoorScript : MonoBehaviour
 {
     public GameObject player;
     public GameObject bossGegner;
+    public Canvas ui;
+    public Camera cam;
+    public Grid grid;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Spieler")
         {
-            Vector3 neuerPlayerPos = new Vector3(-4, 42, 0);
+            Scene bossRaum = SceneManager.GetSceneByName("BossRaum");
+            Scene currentScene = SceneManager.GetSceneByName("SampleScene");
+           /* Vector3 neuerPlayerPos = new Vector3(-4, 42, 0);
             player.transform.position = neuerPlayerPos;
             Camera.main.transform.position = new Vector3(-4, 50, -10);
-            Instantiate(bossGegner, new Vector3(-4, 50, 0), Quaternion.identity); 
+            Instantiate(bossGegner, new Vector3(-4, 50, 0), Quaternion.identity);*/
+            DontDestroyOnLoad(player);
+            DontDestroyOnLoad(ui);
+            DontDestroyOnLoad(cam);
+           // DontDestroyOnLoad(grid);
+            SceneManager.LoadScene("BossRaum", LoadSceneMode.Single);
+            
+            
+
         }
     }
-  
+   
 }
