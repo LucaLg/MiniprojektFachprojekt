e1b5bfe172abe7f180aaa9cf75389015f2cfb3ee
luca <lucal.gno@gmail.com>
2020-11-19 14:52:00 +0100

    FallenTiles
    Fallen Tiles haben eine Animation die bei betreten Aktiviert wird.Steht der Spieler zwei Sekunden auf dem Tile verliert er ein Leben

--- Miniprojekt/Assets/Scripts/Fallen.cs
+++ Miniprojekt/Assets/Scripts/Fallen.cs
@@ -7,45 +7,68 @@ public class Fallen : MonoBehaviour
 {
     public bool isActive;
     private int fallenStatus;
-    public Tilemap tilemap;
+    public Tilemap fallen;
+    public Animator fallenAnimator;
+    private float timerCountDown = 2f;
+    private bool isColliding = false;
+    private bool healthLost = false;
     // Start is called before the first frame update
     void Start()
     {
-        fallenStatus = 1;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Color c=Color.white;
-        if (fallenStatus == 1)
+        if (isColliding)
         {
-            c = Color.yellow;
-        }else if(fallenStatus == 2)
-        {
-            c = Color.red;
+            timerCountDown -= Time.deltaTime;
+            if (timerCountDown < 0)
+            {
+                timerCountDown = 0;
+            }
         }
-
-
-        tilemap.SetTileFlags(new Vector3Int(-15, -6, 0), TileFlags.None);
-        tilemap.SetColor(new Vector3Int(-15, -6, 0), Color.red);
-        tilemap.RefreshAllTiles();
-
-        //StartCoroutine(FallenStatus());
-
     }
-    IEnumerator FallenStatus()
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        yield return new WaitForSeconds(3);
-        if (fallenStatus <2)
+        if(collision.gameObject.tag == "Spieler")
         {
-            fallenStatus++;
+            Debug.Log("Player Entered");
+            
+            isColliding = true;
+            
         }
-        if(fallenStatus == 2)
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Spieler" && isColliding)
         {
-            fallenStatus = 0;
+            fallenAnimator.SetBool("drauf",true);
+            Debug.Log("Countdown not done yet");
+            if (timerCountDown <= 0 && !healthLost)
+            {
+                
+                collision.gameObject.GetComponent<PlayerController>().leben--;
+                healthLost = true;
+            }
         }
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Spieler")
+        {
+            
+            Debug.Log("Player Exited");
+            fallenAnimator.SetBool("drauf", false);
+            isColliding = false;
+            healthLost = false;
+            timerCountDown = 2f;
+
+        }
+    }
+
+
 }
